04a515868bf08ccfc8a4eca61dd2929e4aff7b1b
Pepper Lebeck-Jobe <eljobe@gmail.com>
2024-09-11 17:50:24 +0200

    Minor cleanup
    Remove a straggler refernence to pinentry and change which oh-my-zsh
    packages I am pulling in slightly.

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -330,7 +330,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(chezmoi yaml-mode rust-mode pinentry paradox multi-vterm markdown-mode magit groovy-mode go-mode gnu-elpa-keyring-update diffview crux)))
+   '(chezmoi yaml-mode rust-mode paradox multi-vterm markdown-mode magit groovy-mode go-mode gnu-elpa-keyring-update diffview crux)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
