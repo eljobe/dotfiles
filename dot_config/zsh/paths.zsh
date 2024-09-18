3b6e2347eb012c1e1a3cbf8c3609ce134c9eaee9
Pepper Lebeck-Jobe <eljobe@gmail.com>
2024-09-18 10:36:53 +0200

    Finally get some sanity around cross-system path management.
    This feels more complicated than it needs to be, but I'm now convinced
    that it's not really.

---
+++ dot_config/zsh/paths.zsh
@@ -0,0 +1,75 @@
+# -*-sh-*-
+
+# This file is sourced both at the end of my ~/.zshenv and at the top
+# of my .zshrc.
+#
+# The objective of these commands is to take whatever the system-wide
+# definition of the path is, prepend and append my custom path
+# elements. However, especially in the case of the .zshrc, what is
+# actually happening is a reshuffling of the order of my custom path
+# elements to the front or back of the path. I have to do this
+# because on OS X's system-wide /etc/zprofile it prepends all of the
+# paths from /etc/paths{.d} to whatever I've previously set up in my
+# ~/.zshenv
+
+# Note: The order in which paths are prepended and appended is
+# important.
+
+# Define a function to move a path to the end or simply append the
+# path if it isn't already in the path array.
+append_path() {
+    local dir="$1"
+    # Remove the path from 'path' array
+    path=(${path:#$dir})
+    # Append the path to the end
+    path+=("$dir")
+}
+
+# Define a function to prepend the path. This is trival, but wrappeed
+# in a function to make it more symmetric to appending.
+prepend_path() {
+    local dir="$1"
+    # Since path is typeset -U (unique array) only the first instance
+    # of $dir will survive.
+    path=($dir $path)
+}
+
+# Make path a unique array.
+# This ensures that any value only appears once in the array.
+typeset -U path
+
+# Homebrew locations vary a lot on my machines.
+# Note, it is possible on a machine that both /usr/local and
+# /opt/homebrew will exist. In that case, I want executables in
+# /opt/homebrew to take precedence.
+brewroots=('/usr/local' '/opt/homebrew' '/home/linuxbrew/.linuxbrew')
+for root in $brewroots; do
+    # If the homebrew root exists, add both sbin and bin to the path
+    # whether or not they exist as subdirectories. This is because
+    # brew doctor wants both of them to be on the path in case a new
+    # sbin binary gets installed in the current session.
+    if [ -d "$root" ]; then
+	# sbin before bin so that bin takes relative precedence.
+	prepend_path $root/sbin
+	prepend_path $root/bin
+    fi
+done
+
+# Cargo is rust's wrapper and package manager.
+if [ -f "$HOME/.cargo/env" ]; then
+    prepend_path $HOME/.cargo/bin
+fi
+
+# Foundry is a bunch of tools for use with ethereum development.
+foundrydir="$XDG_CONFIG_HOME/.foundry"
+if [ -d "$foundrydir" ]; then
+    prepend_path $foundrydir/bin
+fi
+unset foundrydir
+
+# Append my $HOME/bin directory.
+append_path $HOME/bin
+
+export PATH
+unset -f append_path
+unset -f prepend_path
