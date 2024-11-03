35bed122fd050665ae1e9c54288d22c2d4c97e3f
Pepper Lebeck-Jobe <eljobe@gmail.com>
2024-11-03 15:48:12 +0100

    Disable copilot, it was freezing emacs

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -382,14 +382,14 @@
   (dirvish-peek-mode) ; Preview files in minibuffer
   (dirvish-side-follow-mode))
 
-(use-package copilot
-  :vc (:url "https://github.com/copilot-emacs/copilot.el"
-            :rev :newest
-            :branch "main")
-  :hook (prog-mode copilot-mode)
-  :config
-  (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
-  (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion))
+;; (use-package copilot
+;;   :vc (:url "https://github.com/copilot-emacs/copilot.el"
+;;            :rev :newest
+;;            :branch "main")
+;;  :hook (prog-mode copilot-mode)
+;;  :config
+;;  (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
+;;  (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion))
 
 (use-package chezmoi
   :init
